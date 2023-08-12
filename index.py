2ec9e96066b7d5fb984fa1ec7dbfa512eda09475
TingAn1212 <105140568+TingAn1212@users.noreply.github.com>
2023-08-12 13:12:00 +0800

    api key testing

--- index.py
+++ index.py
@@ -1,5 +1,20 @@
 from flask import Flask, render_template
+import requests
 import os
+
+def is_api_key_valid(api_key):
+    url = "https://api.openai.com/v1/engines/davinci/completions"
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "prompt": "Test API key",
+        "max_tokens": 1
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    return response.status_code == 200
 
 app = Flask(__name__)
 gpt = os.environ.get('CHAT_GPT')
@@ -10,7 +25,7 @@ else:
 
 @app.route("/")
 def hello_world():
-    return render_template('testing_template.html', key_status=(lambda x: "Chat-GPT Key Found" if x else "Chat-GPT Key Not Found")(keys["chatgpt"]))
+    return render_template('testing_template.html', key_status=(lambda x: "Chat-GPT API key is valid" if x else "Chat-GPT API key is not valid")(is_api_key_valid(keys["chatgpt"])))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8888,debug=True)
