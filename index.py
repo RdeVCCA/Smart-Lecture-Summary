70d803bf2f6423733af915c02dc06e5e6faf8d29
TingAn1212 <105140568+TingAn1212@users.noreply.github.com>
2023-08-12 13:03:06 +0800

    template testing round 1

--- index.py
+++ index.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+gpt = os.environ.get('CHAT_GPT')
+if gpt:
+    keys = {"chatgpt":gpt}
+else:
+    keys = {"chatgpt":os.getenv('CHAT_GPT')}
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('testing_template.html', key_status=(lambda x: "Chat-GPT Key Found" if x else "Chat-GPT Key Not Found")(keys["chatgpt"]))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8888,debug=True)
