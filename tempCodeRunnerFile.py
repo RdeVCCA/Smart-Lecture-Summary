64670ec8a3224c35829dd24e5e546c83f1169249
TingAn1212 <105140568+TingAn1212@users.noreply.github.com>
2024-03-27 17:14:13 +0800

    Created folder placeholder

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,16 +1,1 @@
-import os
-def delete_empty_folders(directory):
-    for root, dirs, files in os.walk(directory, topdown=False):
-        for name in dirs:
-            dir_path = os.path.join(root, name)
-            try:
-                if not os.listdir(dir_path):
-                    try:
-                        os.rmdir(dir_path)
-                        print("Removed empty folder:", dir_path)
-                    except:
-                        print(f"Failed to remove {dir_path}")
-            except:
-                print(f"Failed to list {dir_path}")
-
-delete_empty_folders("upload")
+api_key
